caebb6c07297e60903797a524bc3008da3a13ef8
Bruno Said Alves <86388642+brsaidas@users.noreply.github.com>
2022-11-16 14:25:54 -0300

    Delete prototipo directory

--- prototipo/src/main/java/br/edu/unfei/ecot12/lab10/clone/Igualdade.java
+++
@@ -1,5 +0,0 @@
-package br.edu.unfei.ecot12.lab10.clone;
-
-public interface Igualdade<T extends Igualdade<T>> {
-	public abstract boolean isEqual(T obj);
-}
--- prototipo/src/main/java/br/edu/unfei/ecot12/lab10/clone/Prototipo.java
+++
@@ -1,5 +0,0 @@
-package br.edu.unfei.ecot12.lab10.clone;
-
-public interface Prototipo<T extends Prototipo<T>> {
-	public abstract T clone();
-}
